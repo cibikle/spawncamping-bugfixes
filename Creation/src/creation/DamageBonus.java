b0f529e6436b11591563d9062e43ab609bcc4d92
Christian Bikle <cibikle@gmail.com>
2014-10-10 16:10:03 -0400

    Created DamageBonus class.

---
+++ Creation/src/creation/DamageBonus.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package creation;
+
+/**
+ *
+ * @author cibikle
+ */
+public class DamageBonus {
+   private Dice db;
+   private boolean negativeDB;
+   
+   public DamageBonus(Dice db, boolean negativeDB) {
+      this.db = db;
+      this.negativeDB = negativeDB;
+   }
+
+   /**
+    * @return the db
+    */
+   public Dice getDb() {
+      return db;
+   }
+
+   /**
+    * @return the negativeDB
+    */
+   public boolean isNegativeDB() {
+      return negativeDB;
+   }
+
+   /**
+    * @param db the db to set
+    */
+   public void setDb(Dice db) {
+      this.db = db;
+   }
+
+   /**
+    * @param negativeDB the negativeDB to set
+    */
+   public void setNegativeDB(boolean negativeDB) {
+      this.negativeDB = negativeDB;
+   }
+}
