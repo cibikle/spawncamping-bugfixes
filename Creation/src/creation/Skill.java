84c318209078a04187b3bf748acee9608376aed7
Christian Bikle <cibikle@gmail.com>
2014-10-13 01:21:01 -0400

    Created new Skill class (lacking boolean : checked and encapsulation methods) to support and enforce uncheckable skills (such as Cthulhu Mythos, Sorcery, and Status).

--- Creation/src/creation/Skill.java
+++ Creation/src/creation/Skill.java
@@ -9,14 +9,12 @@ package creation;
  * @author cibikle
  */
 public class Skill {
-   private String name;
-   private int skillLevel;
-   private boolean checked;
+   protected String name;
+   protected int skillLevel;
    
-   public Skill(String name, int skillLevel, boolean checked) {
+   public Skill(String name, int skillLevel) {
       this.name = name;
       this.skillLevel = skillLevel;
-      this.checked = checked;
    }
    
    public String getName(){
@@ -25,18 +23,6 @@ public class Skill {
    
    public int getSkillLevel() {
       return skillLevel;
-   }
-   
-   public boolean getChecked() {
-      return checked;
-   }
-   
-   public void setChecked() {
-      checked = true;
-   }
-   
-   public void clearChecked() {
-      checked = false;
    }
    
    public void setSkillLevel(int skillLevel) {
@@ -50,22 +36,4 @@ public class Skill {
    public String toString() {
       return name+": "+skillLevel;
    }
-   
-/*   public int resolveCheck(int addend) {
-      updateSkillLevelByValue(addend);
-      clearChecked();
-      return skillLevel;
-   }
-   
-   public int autoResolveCheck(int xpBonus) {
-      Dice percentageDice = new Dice(true);
-      int result = percentageDice.rollDice();
-      if(result + xpBonus > skillLevel) {
-         Dice improvementDie = new Dice(1, 6);
-         result = improvementDie.rollDice();
-         updateSkillLevelByValue(result);
-      }
-      clearChecked();
-      return skillLevel;
-   }*/
- }
+}
