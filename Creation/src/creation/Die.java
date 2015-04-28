10d642f520620db76986a715ec1ceccf2f8b463a
Christian Bikle <cibikle@gmail.com>
2015-04-28 01:38:15 -0400

    Changed name of field and constructor parameter "d" to "sizeOfDie". Added methods getSizeOfDie(), getMin(), getMax(), getAvg().

--- Creation/src/creation/Die.java
+++ Creation/src/creation/Die.java
@@ -11,15 +11,31 @@ import java.util.Random;
  * @author cibikle
  */
 public class Die {
-   private int d;
-   private Random r;
+   private final int sizeOfDie;
+   private final Random r;
    
-   public Die(int d) {
-      this.d = d;
+   public Die(int sizeOfDie) {
+      this.sizeOfDie = sizeOfDie;
       r = new Random();
    }
    
+   public int getSizeOfDie() {
+       return sizeOfDie;
+   }
+   
+    public int getMin() {
+        return 1;
+    }
+
+    public int getMax() {
+        return sizeOfDie;
+    }
+
+    public double getAvg() {
+        return (sizeOfDie + 1) / 2;
+    }
+   
    public int roll() {
-      return r.nextInt(d) + 1;
+      return r.nextInt(sizeOfDie) + 1;
    }
 }
