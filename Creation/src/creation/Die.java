f6a98adad797df751de7c98e27b0134ee57278a3
Christian Bikle <cibikle@gmail.com>
2015-04-28 01:51:52 -0400

    Added overloaded getMin(), getMax(), getAvg(), roll() methods to accommodate more flexible addend system (i.e., int addend as an optional parameter).

--- Creation/src/creation/Die.java
+++ Creation/src/creation/Die.java
@@ -11,31 +11,48 @@ import java.util.Random;
  * @author cibikle
  */
 public class Die {
-   private final int sizeOfDie;
-   private final Random r;
-   
-   public Die(int sizeOfDie) {
-      this.sizeOfDie = sizeOfDie;
-      r = new Random();
-   }
-   
-   public int getSizeOfDie() {
-       return sizeOfDie;
-   }
-   
+
+    private final int sizeOfDie;
+    private final Random r;
+
+    public Die(int sizeOfDie) {
+        this.sizeOfDie = sizeOfDie;
+        r = new Random();
+    }
+
+    public int getSizeOfDie() {
+        return sizeOfDie;
+    }
+
     public int getMin() {
         return 1;
+    }
+
+    public int getMin(int addend) {
+        return 1 + addend;
     }
 
     public int getMax() {
         return sizeOfDie;
     }
 
+    public int getMax(int addend) {
+        return sizeOfDie + addend;
+    }
+
     public double getAvg() {
         return (sizeOfDie + 1) / 2;
     }
-   
-   public int roll() {
-      return r.nextInt(sizeOfDie) + 1;
-   }
+
+    public double getAvg(int addend) {
+        return ((sizeOfDie + 1) / 2) + addend;
+    }
+
+    public int roll() {
+        return r.nextInt(sizeOfDie) + 1;
+    }
+
+    public int roll(int addend) {
+        return r.nextInt(sizeOfDie) + 1 + addend;
+    }
 }
