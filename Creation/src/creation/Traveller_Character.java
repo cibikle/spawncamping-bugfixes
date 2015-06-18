ba35d0d881120aebabb8664656fc67d1f3aeda0b
Christian Bikle <cibikle@gmail.com>
2015-06-18 05:01:04 -0400

    Added Traveller_Character.java Need to revise RPG_Character into system-independent superclass for all RPG character types. Added empty and small constructors and calcCharacteristicDMs() method.

---
+++ Creation/src/creation/Traveller_Character.java
@@ -0,0 +1,53 @@
+package creation;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Christian Bikle
+ */
+public class Traveller_Character implements Serializable {
+    private String name;
+    private String playerName;
+    private int[] characteristics;
+    private int[] characteristicsDMs;
+    private String homeworldName;
+    private String[] homeworldCharacteristics;
+    private Skill[] skills;
+    
+    public Traveller_Character() {
+        
+    }
+    
+    public Traveller_Character(int[] characteristics, Skill[] skills) {
+        this.characteristics = characteristics;
+        this.characteristicsDMs = calcCharacteristicDMs(this.characteristics);
+        this.skills = skills;
+    }
+
+    public static int[] calcCharacteristicDMs(int[] characteristics) {
+        int[] dms = new int[characteristics.length];
+        for (int i = 0; i < characteristics.length; i++) {
+            if (characteristics[i] >= 6 && characteristics[i] <= 8) {
+                dms[i] = 0;
+            } else if (characteristics[i] >= 3 && characteristics[i] <= 5) {
+                dms[i] = -1;
+            } else if (characteristics[i] >= 9 && characteristics[i] <= 11) {
+                dms[i] = 1;
+            } else if (characteristics[i] >= 1 && characteristics[i] <= 2) {
+                dms[i] = -2;
+            } else if (characteristics[i] >= 12 && characteristics[i] <= 14) {
+                dms[i] = 2;
+            } else if (characteristics[i] == 0) {
+                dms[i] = -3;
+            } else if (characteristics[i] == 15) {
+                dms[i] = 3;
+            } else {
+                System.err.println("Error in rollForCharacteristics(): "
+                        + characteristics[i] + " is not a valid value.");
+                dms[i] = 0;
+            }
+        }
+        return dms;
+    }
+}
